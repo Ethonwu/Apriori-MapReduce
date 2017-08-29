5d43af0d14d984f086f3423cb07658a33ffed746
a5351998 <a5351998@gmail.com>
2017-08-29 21:03:54 +0800

    Add a little function to test return value and will add decompose function

--- src/Apriori/Apriori.java
+++ src/Apriori/Apriori.java
@@ -1,7 +1,12 @@
 package Apriori;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+import org.apache.log4j.Category;
+import org.apache.log4j.Logger;
+import org.apache.commons.logging.Log;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -14,21 +19,35 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 public class Apriori {
 	public static class TokenizerMapper 
+	
     extends Mapper<Object, Text, Text, IntWritable>{
 
  private final static IntWritable one = new IntWritable(1);
- private Text word = new Text();
 
+ private Text word = new Text();
+ public ArrayList<String> itemsets = new ArrayList<String>();
  public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
    StringTokenizer itr = new StringTokenizer(value.toString());
+   
+   itemsets.add(itr.toString());
+   System.out.println("See here:"+itemsets.toString());
+   int a = 1,r=0;
+   r = add(a);
+   IntWritable ones = new IntWritable(r);
    while (itr.hasMoreTokens()) {
      word.set(itr.nextToken());
-     context.write(word, one);
+     context.write(word,ones);
    }
  }
+ private int add(int n) {
+	 n = n + 90;
+	 return n;
+ }
+ 
 }
+	
 
-public static class IntSumReducer 
+ public static class IntSumReducer 
     extends Reducer<Text,IntWritable,Text,IntWritable> {
  private IntWritable result = new IntWritable();
 
@@ -39,10 +58,14 @@ public static class IntSumReducer
    for (IntWritable val : values) {
      sum += val.get();
    }
-   result.set(sum);
+   if (sum >= 2) {
+	   result.set(sum);   
+   }
+   
    context.write(key, result);
  }
 }
+ 
 public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
@@ -60,5 +83,7 @@ public static void main(String[] args) throws Exception {
     FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
     FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
+    
+   
   }
 }
